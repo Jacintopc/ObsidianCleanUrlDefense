6f9d19b69b449bb1f672b6d3c910700ca5f0a388
Jacinto Pérez <jacintopc@hotmail.com>
2023-07-23 10:52:05 +0200

    count lines

--- main.ts
+++ main.ts
@@ -1,4 +1,42 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { spawn } from "child_process";
+import {Plugin} from "obsidian";
+
+export default class ExamplePlugin extends Plugin {
+	statusBarTextElement: HTMLSpanElement;
+	onload() {
+		//console.log("hola Jacinto");
+		this.statusBarTextElement = this.addStatusBarItem().createEl('span');
+		//this.statusBarTextElement.textContent ="ya estamos";
+		this.readActiveFileAndUpdateLineCount();
+
+		this.app.workspace.on('active-leaf-change',async () => {
+			this.readActiveFileAndUpdateLineCount();
+		});
+		
+		this.app.workspace.on('editor-change',editor=>{
+			const content = editor.getDoc().getValue();
+			this.updateLineCount(content);
+		})
+	}
+	private updateLineCount(fileContent?:string){
+		const count =fileContent ? fileContent.split(/\r\n|\r|\n/).length:0;
+		const linesWord = count ===1?"line":"lines";
+		this.statusBarTextElement.textContent=`${count} ${linesWord}`;
+
+	}
+	private async readActiveFileAndUpdateLineCount(){
+	const file=this.app.workspace.getActiveFile();
+	if (file) {
+		const content= await this.app.vault.read(file);
+		//console.log(content);
+		this.updateLineCount(content);
+	} else {
+		this.updateLineCount(undefined)
+	}
+	}
+}
+
+/* import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
@@ -132,3 +170,4 @@ class SampleSettingTab extends PluginSettingTab {
 				}));
 	}
 }
+ */
